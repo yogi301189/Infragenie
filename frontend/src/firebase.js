ff71a84c0847650bbfb203e95daf3c87eaa7ab39
unknown <yogifst@gmail.com>
2025-07-14 10:22:27 +0530

    fresw

---
+++ frontend/src/firebase.js
@@ -0,0 +1,19 @@
+// src/firebase.js
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
+// Firebase config from environment variables
+const firebaseConfig = {
+  apiKey: "AIzaSyBWjcLCx7vio8uxrFrWsJjFVQwgRTraedU",
+  authDomain: "infragenie-4e256.firebaseapp.com",
+  projectId: "infragenie-4e256",
+  storageBucket: "infragenie-4e256.firebasestorage.app",
+  messagingSenderId: "928280669851",
+  appId: "1:928280669851:web:cf5f762ef2ddc0f729a03e"
+};
+
+// Initialize Firebase app
+const app = initializeApp(firebaseConfig);
+
+// Export Firebase Authentication instance
+export const auth = getAuth(app);
